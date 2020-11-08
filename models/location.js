e94e25cbc50418e652b4e355ff569b7679af3056
taylorschmidt <taylorhillschmidt@gmail.com>
2020-11-08 08:51:25 -0700

    added locations model and associations and migrated

---
+++ models/location.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class location extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.location.belongsTo(models.user)
+    }
+  };
+  location.init({
+    latitude: DataTypes.INTEGER,
+    longitude: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'location',
+  });
+  return location;
+};
--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      models.user.hasMany(models.location)
     }
   };
   user.init({

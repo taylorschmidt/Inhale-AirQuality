79f7dc9f3e3253196bd19661c6b89088af909c6c
taylorschmidt <taylorhillschmidt@gmail.com>
2020-11-18 18:36:25 -0700

    changed styling

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 
 //set up style.css
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 //body parser middleware that makes req.body work
 app.use(express.urlencoded({extended:false}))
